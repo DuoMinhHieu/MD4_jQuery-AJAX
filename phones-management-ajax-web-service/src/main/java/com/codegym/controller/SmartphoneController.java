b3b8f899ebcce2ad491f6ea46aadff8efd402509
DuoMinhHieu <dmh6774@gmail.com>
2024-05-29 08:47:13 +0700

    sua frontend

--- phones-management-ajax-web-service/src/main/java/com/codegym/controller/SmartphoneController.java
+++ phones-management-ajax-web-service/src/main/java/com/codegym/controller/SmartphoneController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin("*")
+@CrossOrigin()
 @RequestMapping("/api/smartphones")
 public class SmartphoneController {
     @Autowired
